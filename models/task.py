5c3dbe9a4bc9ee504e96714f861a23f484c9b06a
Daniela Echauri <daniela.delgado.echauri@upa.edu.py>
2022-09-15 09:12:15 -0400

    Commit funcional

--- models/task.py
+++ models/task.py
@@ -1,17 +1,27 @@
-class TaskModel (): #Definicion de una clase
+from db import db
 
 
-    #definicion de los atributos de una clase
-    id = int
-    description = str
+class TaskModel(db.Model): #Definicion de una clase
+    __tablename__ = 'task' #para identificar la tabla en la db
 
-    def __init__(self, id, description): #definicion de un constructor para una clase. Self en el objeto, seria el THIS de java
+    id = db.Column(db.Integer, primary_key=True)
+    descrip = db.Column(db.String)
+    status =db.Column(db.String)
+
+    def __init__(self, id, descrip, status): #definicion de un constructor para una clase. Self en el objeto, seria el THIS de java
         self.id = id
-        self.description = description
+        self.descrip = descrip
+        self.status = status
     
     def json (self,depth =0):
         json ={
             'id' : self.id,
-            'description' : self.description
+            'descrip' : self.descrip,
+            'status' :self.status
         }
         return json
+    
+    @classmethod
+    def find_by_id(cls, id):
+        return cls.query.filter_by(id=id).first() # el filter by lo que hace es recibir un parametro
+
--- resources/task.py
+++ resources/task.py
@@ -6,11 +6,12 @@ class Task(Resource):
 
     parser = reqparse.RequestParser() #manejador de peticiones de http
     parser.add_argument('id',type=int)
-    parser.add_argument('description', type = str)
+    parser.add_argument('descrip', type = str)
+    parser.add_argument('status', type = str)
     
     @swag_from('../swagger/get_task.yaml')
     def get(self, id): # el objetivo es que si yo hago un id 1 con la tarea que me lo traiga
-        tarea = TaskModel(1, "Hola Mundo") #objeto de prueba
+        tarea = TaskModel.find_by_id (id)
         if tarea:
             return tarea.json() #traeme el json que representa el id de mi tarea
         return {'mensaje': "No se encuentra la tarea"},404  #codigos de error
